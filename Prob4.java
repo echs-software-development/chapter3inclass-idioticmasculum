49cce1af8878de96565f82ba344497882faee421
Simi <73349362+idioticmasculum@users.noreply.github.com>
2020-12-03 15:21:22 +0000

    Didn't exactly finish prob4 but manage to get at least 1 error instead of 5 like before. Take a look and see what's wrong with it I suppose.

--- Prob4.java
+++ Prob4.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class Prob4  {
   public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is the temperature outside?: ");
+
+    double temp = scan.nextDouble();
+
+    if (temp < 99 || (temp > 102) 
+      System.out.println("WARNING");
+  
+    else{
+      System.out.println("Temperature is OK");
+    }
     
-
-
     
   }
 }
